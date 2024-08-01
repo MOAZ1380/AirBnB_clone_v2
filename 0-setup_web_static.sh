4604ceb9af231f03e599e6d13b90768003bd184f
moaz <moaz469654@gmail.com>
2024-08-01 14:54:03 -0700

    moaz sau

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
-# setup web static server
+# Script to set up web servers for the deployment of web_static
 
-sudo apt-get update
-sudo apt-get install -y nginx
+# Install Nginx if it is not already installed
+if ! dpkg -l | grep -q nginx; then
+    sudo apt update
+    sudo apt install -y nginx
+fi
+
+# Create necessary directories
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
+
+# Create a fake HTML file
 echo "<html>
   <head>
   </head>
@@ -12,7 +19,19 @@ echo "<html>
     Holberton School
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
+
+# Create a symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data
-sudo sed -i "/listen 80 default_server;/ a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
-sudo service nginx restart
+
+# Give ownership to the ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update the Nginx configuration to serve the content
+sudo sed -i '/server_name _;/a \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+
+# Restart Nginx to apply the changes
+sudo systemctl restart nginx
+
+# Exit successfully
+exit 0
+
