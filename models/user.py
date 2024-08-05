451e3a82ecb638fdde18fcf65ec8d1beb54029fe
Moaz Sayed <moaz469654@gmail.com>
2024-08-05 23:45:09 +0200

    8. DBStorage - Place

--- models/user.py
+++ models/user.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """ class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+
+class User(BaseModel, Base):
     """ class """
-    # print("class user")
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+    places = relationship("Place", back_populates ='user', cascade="all, delete, delete-orphan")
