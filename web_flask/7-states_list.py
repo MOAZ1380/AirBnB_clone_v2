308b38bd61a70223239a073ea4d26231dba291c9
Moaz Sayed <moaz469654@gmail.com>
2024-08-08 21:30:19 +0200

    moazsayed

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,22 +1,33 @@
 #!/usr/bin/python3
-""" Starts a Flask web application """
-from flask import Flask, render_template
+"""
+A script that starts a Flask web application:
+"""
+
+from flask import Flask
 from models import storage
-from models.state import State
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    """ Remove the current SQLAlchemy Session """
-    storage.close()
 
 @app.route('/states_list', strict_slashes=False)
-def states_list():
-    """ Displays a list of all State objects """
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda x: x.name)
-    return render_template('7-states_list.html', states=sorted_states)
+def states_list_route():
+    """
+    List states: display a HTML page: (inside the tag BODY)
+    Returns:
+        html: template that lists all states sort by name A->Z
+    """
+    states = storage.all("State").values()
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def close_db(exception=None):
+    """
+    After each request remove the current SQLAlchemy Session:
+    """
+    storage.close()
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
