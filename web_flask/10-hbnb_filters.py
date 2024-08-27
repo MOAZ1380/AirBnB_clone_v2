cffc0d56a86410add2d9318b01f0b5bdd49dda95
Moaz Sayed <moaz469654@gmail.com>
2024-08-27 10:59:40 +0200

    finally

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -17,11 +17,13 @@ def teardown_db(exception):
 @app.route('/hbnb_filters', strict_slashes=False)
 def hbnb_filters():
     """ hbnb_filters """
-    states = storage.all("State").values()
-    amenity = storage.all("Amenity").values()
-    return render_template('7-states_list.html', States=states, Amenity=amenity)
+    data = {
+        "states": storage.all("State").values(),
+        "amenities": storage.all("Amenity").values()
+    }
+    return render_template("10-hbnb_filters.html", models=data)
 
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5050)
